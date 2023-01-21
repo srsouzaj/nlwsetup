cf7a8caf404b7df9fa405feb59978ed62ec7cb83
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-01-21 19:56:42 -0300

    feat: backend/frontend initial

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import { Habits } from "./components/Habit"
+import "./styles/global.css"
+
+export const App = () => {
+
+  return (
+    <Habits completed={3} />
+
+  )
+}
---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,10 @@
+interface HabitInterface {
+    completed: number
+}
+
+
+export const Habits = ({ completed }: HabitInterface) => {
+    return (
+        <div className="bg-zinc-900 w-10 h-10 text-white rounded">{completed}</div>
+    )
+}
