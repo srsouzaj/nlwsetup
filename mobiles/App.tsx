7c447f05f41a1102bbe562f495d5b2a4099692f6
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-01-22 14:52:48 -0300

    feat: grid habits

--- mobiles/App.tsx
+++ mobiles/App.tsx
@@ -1,36 +1,34 @@
+import './src/lib/dayjs';
 
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from "@expo-google-fonts/inter"
+import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter';
+
 import { Loading } from './src/components/Loading';
-
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold
-  })
-
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
   if (!fontsLoaded) {
-    return <Loading />
+    return (
+      <Loading />
+    );
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090a',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: "red",
-    fontFamily: "Inter_800ExtraBold"
-  }
-});
