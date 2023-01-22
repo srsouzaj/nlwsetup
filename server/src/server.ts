724113c4289ee893bea34ff228da72698d953240
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-01-22 13:05:00 -0300

    feat: create routes and seeds

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,14 @@
 import Fastify from 'fastify'
 import cors from "@fastify/cors"
-import { PrismaClient } from "@prisma/client"
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
-app.get("/", async () => {
-    const habits = await prisma.habit.findMany()
-    return habits
-}),
+app.register(cors)
+app.register(appRoutes)
 
-
-    app.listen({
-        port: 3333
-    }).then(() => {
-        console.log("HTTP server running")
-    })
+app.listen({
+    port: 3333
+}).then(() => {
+    console.log("HTTP server running")
+})
