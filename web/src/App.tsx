200a8d826b481b201bf61cd815e53b397130938b
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-01-22 13:20:40 -0300

    feat: create grid habits

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,17 @@
-import { Habits } from "./components/Habit"
+import { HabitDay } from "./components/HabitDay"
+import { Header } from "./components/Header"
+import { SummaryTable } from "./components/SummaryTable"
 import "./styles/global.css"
 
 export const App = () => {
 
   return (
-    <Habits completed={3} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
 
   )
 }
