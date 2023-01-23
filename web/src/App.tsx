825acf31a4006fc6bf1951c456e0337993d7f1f5
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-01-23 17:12:43 -0300

    feat: api integration

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,8 @@
-import { HabitDay } from "./components/HabitDay"
+import "./styles/global.css"
+import './lib/dayjs'
+
 import { Header } from "./components/Header"
 import { SummaryTable } from "./components/SummaryTable"
-import "./styles/global.css"
 
 export const App = () => {
 
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,5 +1,9 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "./HabitDay";
+
 const weekDays = [
     'D',
     'S',
@@ -12,7 +16,24 @@ const weekDays = [
 const summaryDates = generateDatesFromYearBeginning()
 const minimumSummaryDatesSize = 18 * 7 // 18 weeks
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
+type Summary = {
+    id: string;
+    date: string;
+    amount: number;
+    completed: number;
+}[]
+
 export const SummaryTable = () => {
+
+    const [summary, setSummary] = useState<Summary>([])
+
+    useEffect(() => {
+        api.get('summary').then(response => {
+            setSummary(response.data)
+        })
+    }, [])
+
     return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -27,17 +48,23 @@ export const SummaryTable = () => {
                     )
                 })}
             </div>
+
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map((date) => {
+
+                    const dayInSummary = summary.find(day => {
+                        return dayjs(date).isSame(day.date, 'day')
+                    })
+
                     return (
                         <HabitDay
                             key={date.toString()}
-                            amount={5}
-                            completed={Math.round(Math.random() * 5)}
+                            date={date}
+                            amount={dayInSummary?.amount}
+                            completed={dayInSummary?.completed}
                         />
                     )
                 })}
-
                 {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
                     return (
                         <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
