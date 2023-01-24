43d7938d6a88d5cd94f8a2e0c8cf6e86fd0a8804
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-01-23 21:52:50 -0300

    fix: habits

--- mobiles/src/components/Checkbox.tsx
+++ mobiles/src/components/Checkbox.tsx
@@ -1,13 +1,13 @@
 import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
 import { Feather } from '@expo/vector-icons'
 import colors from "tailwindcss/colors";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
 
 interface CheckboxInterface extends TouchableOpacityProps {
     title: string;
     checked?: boolean;
 }
-
-export function Checkbox({ title, checked = false, ...rest }: CheckboxInterface) {
+export const Checkbox = ({ title, checked = false, ...rest }: CheckboxInterface) => {
     return (
         <TouchableOpacity
             activeOpacity={0.7}
@@ -16,17 +16,20 @@ export function Checkbox({ title, checked = false, ...rest }: CheckboxInterface)
         >
             {checked
                 ?
-                <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+                <Animated.View
+                    className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+                    entering={ZoomIn}
+                    exiting={ZoomOut}
+                >
                     <Feather
                         name="check"
                         size={20}
                         color={colors.white}
                     />
-                </View>
+                </Animated.View>
                 :
                 <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
             }
-
             <Text className="text-white text-base ml-3 font-semibold">
                 {title}
             </Text>
